39a44a29cc98c9e29e6a59db3e3e312d87c36022
Jhonty <119353020+jhonty007@users.noreply.github.com>
2025-01-19 23:14:00 +0530

    Add files via upload

---
+++ data/items.js
@@ -0,0 +1,44 @@
+const items = [
+    {
+        id: '001',
+        image: 'images/1.jpg',
+        item_name: 'Hari Arpna',
+        original_price: 299,
+        current_price: 100,
+        discount_percentage: 67,
+        return_period: 14,
+        delivery_date: '22-01-2024',
+        rating: {
+            stars: 4.5,
+            count: 1400,
+        },
+    },
+    {
+        id: '002',
+        image: 'images/2.jpg',
+        item_name: 'Hari Arpna',
+        original_price: 249,
+        current_price: 100,
+        discount_percentage: 60,
+        return_period: 14,
+        delivery_date: '22-01-2024',
+        rating: {
+            stars: 4.3,
+            count: 24,
+        },
+    },
+    {
+        id: '003',
+        image: 'images/3.jpg',
+        item_name: 'Hari Arpna',
+        original_price: 799,
+        current_price: 499,
+        discount_percentage: 38,
+        return_period: 14,
+        delivery_date: '22-01-2024',
+        rating: {
+            stars: 4.3,
+            count: 24,
+        },
+    }
+];
